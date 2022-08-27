e305b12601b498542dd3f8d576c4719bf12f8d19
brian <briansw@gmail.com>
2022-08-27 07:48:26 -0400

    initial commit

---
+++ code.ts
@@ -0,0 +1,54 @@
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__, { width: 294, height: 268, title: "16x scale" });
+console.log('hi');
+
+// Trigger any time an element is selected
+figma.on("selectionchange", () => { 
+  
+  const node = figma.currentPage.selection[0];
+  
+  if (node !== undefined) {
+    figma.ui.postMessage({
+      width: node.width,
+      height: node.height
+    });
+  }
+
+})
+
+figma.ui.onmessage = msg => {  
+  if (msg.type === 'add') {
+    const nodes: SceneNode[] = [];
+    const bounds = figma.viewport.bounds;
+    const rect = figma.createRectangle();
+
+    var scaled_width = (msg.width * 16),
+        scaled_height = (msg.height * 16);
+
+    rect.resize(scaled_width, scaled_height);
+    rect.x = bounds.x;
+    rect.y = bounds.y;
+
+    rect.fills = [{type: 'SOLID', color: {r: 0, g: 1, b: 0}}];
+    figma.currentPage.appendChild(rect);
+    nodes.push(rect);
+
+    figma.currentPage.selection = nodes;
+    // figma.viewport.scrollAndZoomIntoView(nodes);
+    
+  } else if (msg.type === 'edit') {
+    
+    // resize selected rect to new dims
+
+    var scaled_width  = (msg.width * 16), 
+        scaled_height = (msg.height * 16);
+
+    const rect = figma.currentPage.selection[0];
+    rect.resize(scaled_width, scaled_height)
+    
+  }
+
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  // figma.closePlugin();
+};
